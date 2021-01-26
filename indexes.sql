5ffa3f1252a1310f482e17c2314b19d146c13648
Peter Ly <peter.ly1@hotmail.com>
2021-01-26 22:02:24 +0000

    Create indexes.sql

---
+++ indexes.sql
@@ -0,0 +1,12 @@
+CREATE Clustered Index IX_testruns_runs
+ON testruns (runs DESC)
+
+CREATE Clustered Index IX_player_names
+ON players (name ASC)
+
+CREATE Clustered Index IX_venue_names
+ON venues (name ASC)
+
+CREATE Clustered Index IX_umpire_names
+ON umpires (name ASC)
+
